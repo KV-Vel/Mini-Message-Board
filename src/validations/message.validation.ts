beea1cda2dba0b96d5a4aef1a1efacbb09aca87f
SprngVel <kraevvalentin98@gmail.com>
2026-05-25 21:05:56 +1000

    feat: add backend validation of user input

---
+++ src/validations/message.validation.ts
@@ -0,0 +1,31 @@
+import { body } from "express-validator";
+
+const strTypeErr = "should be a string";
+const notEmptyErr = "field cannot be empty";
+
+export const messageValidation = [
+    body("user")
+        .exists()
+        .withMessage("Name field is required")
+        .notEmpty()
+        .withMessage(`Name ${notEmptyErr}`)
+        .trim()
+        .isString()
+        .withMessage(`Name ${strTypeErr}`)
+        .isLength({ min: 2, max: 32 })
+        .withMessage("Name cannot be bigger than 32 and smaller than 2 characters")
+        .escape()
+        .bail({ level: "request" }),
+    body("message")
+        .exists()
+        .withMessage("Message field is required")
+        .notEmpty()
+        .withMessage(`Message ${notEmptyErr}`)
+        .trim()
+        .isString()
+        .withMessage(`Message ${strTypeErr}`)
+        .isLength({ min: 2, max: 200 })
+        .withMessage("Name cannot be bigger than 200 and smaller than 2 characters")
+        .escape()
+        .bail({ level: "request" }),
+];
