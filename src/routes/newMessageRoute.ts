4b25b7d7a671f53a671b0eaef290fb6e123a8e77
SprngVel <kraevvalentin98@gmail.com>
2026-05-25 21:11:41 +1000

    feat: included middleware validation in route

--- src/routes/newMessageRoute.ts
+++ src/routes/newMessageRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { messageValidation } from "../validations/message.validation.ts";
 import postsController from "../controllers/postsController.ts";
 const newMessageRoute = Router();
 
 newMessageRoute.get("/", (req, res) => res.render("pages/new"));
-newMessageRoute.post("/", postsController.addPost);
+newMessageRoute.post("/", messageValidation, postsController.addPost);
 
 export default newMessageRoute;
